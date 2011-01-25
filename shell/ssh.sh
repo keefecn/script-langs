7bf3459d9f8dde8bbc9c11ae90cf62467b5a8961
Denny Wu <wuqifu@gmail.com>
2011-01-25 21:23:33 +0800

    update ssh.sh

--- shell/ssh.sh
+++ shell/ssh.sh
@@ -1,13 +1,31 @@
 #! /bin/bash
 #open-ssh: ssh-keygen -t rsa
-if [ $# -lt "1" ]
-then
-host=192.168.1.170
-#  echo usage: $0 [hostname] [port]
-#  exit 1;
-else
-host=$1
+#cat .ssh/id_rsa.pub | ssh qfwu@61.145.124.32 -p 37856 "cat - >>.ssh/authorized_keys"
+if [ $# -lt "1" ]; then
+  echo usage: $0 [hostname_code_freq]
+  exit 1;
+else 
+{
+  if [ $1 = "3" ]; then
+    # newspider
+    host=61.145.124.165;
+    user=bj-qfwu
+  elif [ $1 = "5" ]; then
+    # novel front 
+    host=61.145.124.32;
+    user=qfwu
+  elif [ $1 = "10" ]; then
+    # yyus test
+    host=61.145.124.165;
+    user=qfwu
+  else
+    # hanwei host
+    host=119.161.158.21;
+    user=wuqifu
+  fi
+}    
 fi
-user=wuqifu
-port=10088
+
+port=37856
 ssh -p $port $host -l $user
+
