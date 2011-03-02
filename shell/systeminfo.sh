d8c7ba516262b0f0e702db6f2f07b819fa74058c
Denny Wu <wuqifu@gmail.com>
2011-03-02 09:47:06 +0800

    system info

--- shell/systeminfo.sh
+++ shell/systeminfo.sh
@@ -2,9 +2,11 @@
 #@author Denny
 #@date 2011-2-16
 #! /bin/bash
+# imp:  strace [cmd]  //Eg: strace uptime
 
 # cpu.sh
-# tools: sar, mpstat
+# tools: sar-System Activity Reporter, mpstat	
+# imp: /proc/stat    = 100   *£¨user   +   nice   +   system£©/£¨user   +   nice   +   system   +   idle£©    
 idle=`/usr/bin/mpstat 1 1 | /bin/grep Average | awk '{print $9}'`
 used=`echo "100 - $idle" | /usr/bin/bc -l -s` 
 echo $used 
@@ -21,12 +23,18 @@ iostat -x /dev/sda|grep sda |awk '{print $9}'
 
 # load.sh
 # tools: uptime
+# imp: /proc/loadavg
 uptime |awk -Faverage '{print $2}'|awk '{print $2}'|awk -F, '{print $1}'
 
 # mem.sh
-# tools: free
+# tools: free, vmstat
+# imp: /proc/meminfo   = 100   *   (cmem   /   umem)  
+#	/proc/vmstat
 /usr/bin/free |grep Mem|awk '{print $4/$2*100}'
 /usr/bin/free |grep Swap|awk '{print $4/$2*100}'
+# application momory utilization ratio
+/usr/bin/free |grep Mem|awk '{print ($3-$7_$8)/$2*100}'
+
 
 # netconn.sh
 # tools: netstat
@@ -36,3 +44,8 @@ echo $tem
 # tcpstate.sh
 # tools: netstat
 netstat -n | awk '/^tcp/ {++S[$NF]} END {for(a in S) print a, S[a]}'
+
+# ulimit.sh
+# tools: ulimit, lsof
+# imp:  /etc/security/limits.conf   /proc/sys/fs/file-max
+ulimit -a
