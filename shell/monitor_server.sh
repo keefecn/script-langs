815477be025266c5e67c54a9e3fdb1fb6088cab2
Denny Wu <wuqifu@gmail.com>
2011-04-15 16:39:30 +0800

    update monitor proc

--- shell/monitor_server.sh
+++ shell/monitor_server.sh
@@ -16,14 +16,14 @@ do
         #pid=$(ps -ef | grep $PROC_NAME |grep -v grep| awk '{print $2}' | head -1) 
         pid=`pgrep $PROC_NAME`
         if [ -z $pid  ]; then
-                echo "["$i"]$PROC_NAME ok...." >/tmp/monitor_server.tmp
-        else
+		# no pid
                 echo "$PROC_NAME is down!!! and restart it now......"  >$LOG 2>&1
                 $START_SCRIPT >>$LOG 2>&1
                 restartTime=`date '+%F %T'` ;
                 echo "TIME: $restartTime" >>$LOG 2>&1
                 mail -s "$PROC_NAME Alert"  $EMAIL_NAME < $LOG
-                #TIME: $restartTime
+        else
+                echo "["$i"]$PROC_NAME ok...." >/tmp/monitor_server.tmp
 #mailtmp
         fi  
         sleep 5;
