4f17165f6392d6bafc06a0d8ae92e0c01752e87b
Denny Wu <wuqifu@gmail.com>
2011-06-09 10:19:25 +0800

    dos2unix

--- shell/convertFileFormat.sh
+++ shell/convertFileFormat.sh
@@ -1,4 +1,17 @@
 #! /bin/sh
-# to convert dos format file to unix: "\r\n"-->"\n"
+# @brief:  convert file format:  
+#   dos format file to unix us: "\r\n"-->"\n"
+
+# method 1: cmd--tr 
 cat $1 |tr -d "\r" > $1.new
 mv $1.new $1
+
+# method 2: vim---set fileformat=dos/unix
+
+# method 3: cmd--dos2unix,unix2dos
+# convert directory
+find -iname "*.py" |xargs dos2unix
+for f in *.txt 
+do
+    dos2unix $f
+done
