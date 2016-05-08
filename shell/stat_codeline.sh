2ee9f945ddc5fa32aacc7b17e8e9f81a4cdaa842
Denny Wu <wuqifu@gmail.com>
2016-05-08 20:44:03 +0800

    mv some code refer to toolkit,such as codeformat/clean/stat_codeline

--- shell/stat_codeline.sh
+++
@@ -1,6 +0,0 @@
-# statics codeline, include *.cpp, *.h, no include spaceline
-# ccs-idx-2: 14421, 3347
-# /bin/bash
-DIRNAME=$1
-find $DIRNAME -iname "*.cpp" |xargs cat |grep -v ^$|wc -l
-find $DIRNAME -iname "*.h" |xargs cat |grep -v ^$|wc -l
