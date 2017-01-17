5378fbe95f9e28937956ea34165573ed9a36d6ec
keefe Wu <wuqifu@gmail.com>
2017-01-17 13:33:59 +0800

    update readme

---
+++ README.md
@@ -0,0 +1,11 @@
+## script
+
+汇总了各种语言的常用脚本。
+* dos：windows平台下的批处理命令
+* shell
+* js~ javascript
+* perl
+* php
+* python
+* ruby
+* toolkit: 各种小工具集合。如code_refer、配置文件etc
--- toolkit/code_refer/git-pull.sh
+++ toolkit/code_refer/git-pull.sh
@@ -4,18 +4,24 @@
 #author: denny, wuqifu@gmail.com
 #date: 2011-01-22
 
-SUBDIRS=`ls -d */ | grep -v 'bin'`
-#SUBDIRS='gflags icethread'
+SUBDIRS=`ls -d */ | grep -v 'keefes'`
+DIS_SUBDIRS='keefes bin'
 
 do_pull()
 {
- for subdir in $SUBDIRS
- do
-  echo ''
-  echo '--'$subdir
-  #( cd $subdir; git pull )
-  ( cd $subdir; git status && git pull )
- done
+  for subdir in $SUBDIRS
+  do
+    case subdir in
+      $DIS_SUBDIRS)
+  	continue
+	;;
+    esac
+
+    echo ''
+    echo '--'$subdir
+    #( cd $subdir; git add *.md && git commit -m 'update readme' -a  && git push && git status)
+    ( cd $subdir; git status && git pull )
+  done
   echo ''
 }
 
