07bdad921b9fc988ff6ada132490f78861bbd746
keefe Wu <wuqifu@gmail.com>
2017-02-19 21:17:38 +0800

    mv tookit/captcha as new repository

--- toolkit/captcha/generate/form.php
+++
@@ -1,7 +0,0 @@
-<form action="post.php" method="POST" onsubmit="return chkinput(this)">
-用户名：<input type="text" name="user"/><br>
-密码：<input type="password" name="pwd"/><br>
-验证码：<input type="text" name="check"/><img src="checkcode.php"><br>
-<input type="submit" value="submit"/>
-</form>
-
