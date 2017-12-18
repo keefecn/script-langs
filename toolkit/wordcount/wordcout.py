3bbb1a327cbcbe35874e0879062f36049c93eccc
Keefe Wu <wuqifu@gmail.com>
2017-12-18 22:41:35 +0800

    update wordcount and virtualenv

--- toolkit/wordcount/wordcout.py
+++ toolkit/wordcount/wordcout.py
@@ -1,9 +1,63 @@
 #!/usr/bin/env pyspark
-
+# -*- coding: utf-8 -*-
+'''
+@file: wordcount.py
+@summary: 
+@author: Keefe Wu
+@since: 2017/11/5
+@note:
 # for spark
 # hdfs url: hdfs://localhost:9000/user/denny/README.md
+@example:
 text_file = sc.textFile("hdfs://...")
 counts = text_file.flatMap(lambda line: line.split(" ")) \
-             .map(lambda word: (word, 1)) \
-             .reduceByKey(lambda a, b: a + b)
+    .map(lambda word: (word, 1)) \
+    .reduceByKey(lambda a, b: a + b)
 counts.saveAsTextFile("hdfs://...")
+@usage: ./bin/spark-submit  --master local[2] wordcount.py  
+'''
+
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from __future__ import print_function
+
+import sys
+from operator import add
+
+from pyspark.sql import SparkSession
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: wordcount <file>", file=sys.stderr)
+        exit(-1)
+
+    spark = SparkSession\
+        .builder\
+        .appName("PythonWordCount")\
+        .getOrCreate()
+
+    lines = spark.read.text(sys.argv[1]).rdd.map(lambda r: r[0])
+    counts = lines.flatMap(lambda x: x.split(' ')) \
+                  .map(lambda x: (x, 1)) \
+                  .reduceByKey(add)
+    output = counts.collect()
+    for (word, count) in output:
+        print("%s: %i" % (word, count))
+
+    spark.stop()
--- toolkit/wordcount/wordcout.scala
+++ toolkit/wordcount/wordcout.scala
@@ -8,3 +8,20 @@ val counts = textFile.flatMap(line => line.split(" "))
                  .map(word => (word, 1))
                  .reduceByKey(_ + _)
 counts.saveAsTextFile("hdfs://...")
+
+import org.apache.spark._
+import SparkContext._
+object WordCount {
+  def main(args: Array[String]) {
+    if (args.length != 3 ){
+      println("usage is org.test.WordCount <master> <input> <output>")
+      return
+    }
+    val sc = new SparkContext(args(0), "WordCount",
+    System.getenv("SPARK_HOME"), Seq(System.getenv("SPARK_TEST_JAR")))
+    val textFile = sc.textFile(args(1))
+    val result = textFile.flatMap(line => line.split("\\s+"))
+        .map(word => (word, 1)).reduceByKey(_ + _)
+    result.saveAsTextFile(args(2))
+  }
+}
